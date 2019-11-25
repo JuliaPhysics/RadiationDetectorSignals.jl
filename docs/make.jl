4f996d8ca5f48cd90dfcca9bd5c30992125cb135
Oliver Schulz <oschulz@mpp.mpg.de>
2019-11-25 15:39:40 +0100

    Update to Documenter v0.24

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 # Use
 #
-#     DOCUMENTER_DEBUG=true julia --color=yes make.jl local [fixdoctests]
+#     DOCUMENTER_DEBUG=true julia --color=yes make.jl local [nonstrict] [fixdoctests]
 #
 # for local builds.
 
@@ -10,17 +10,21 @@ using RadiationDetectorSignals
 makedocs(
     sitename = "RadiationDetectorSignals",
     modules = [RadiationDetectorSignals],
-    format = :html,
+    format = Documenter.HTML(
+        prettyurls = !("local" in ARGS),
+        canonical = "https://JuliaPhysics.github.io/RadiationDetectorSignals.jl/stable/"
+    ),
     pages=[
         "Home" => "index.md",
         "API" => "api.md",
         "LICENSE" => "LICENSE.md",
     ],
     doctest = ("fixdoctests" in ARGS) ? :fix : true,
-    html_prettyurls = !("local" in ARGS),
-    html_canonical = "https://JuliaPhysics.github.io/RadiationDetectorSignals.jl/stable/",
+    linkcheck = ("linkcheck" in ARGS),
+    strict = !("nonstrict" in ARGS),
 )
 
 deploydocs(
-    repo = "github.com/JuliaPhysics/RadiationDetectorSignals.jl.git"
+    repo = "github.com/JuliaPhysics/RadiationDetectorSignals.jl.git",
+    forcepush = true
 )
